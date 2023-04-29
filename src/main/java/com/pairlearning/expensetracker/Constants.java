e988b30411a26edac0d4c019b0e7800346710d80
Paula Viloria <paulasvl12@gmail.com>
2023-04-29 14:40:22 -0400

    first commit

---
+++ src/main/java/com/pairlearning/expensetracker/Constants.java
@@ -0,0 +1,9 @@
+package com.pairlearning.expensetracker;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "expensetrackerapikeyformytrackerapispringproject";
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+
+
+}
---
+++ src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
@@ -0,0 +1,44 @@
+package com.pairlearning.expensetracker;
+
+import com.pairlearning.expensetracker.filters.AuthFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ExpenseTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
+		config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return registrationBean;
+	}
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
+}
